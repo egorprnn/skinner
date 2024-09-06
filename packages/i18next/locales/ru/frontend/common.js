a67cfefa9019447757537a5c3b6b50d6f737a0ae
Egor Pronin <egor.pronin@vk.team>
2024-09-07 03:21:20 +0900

    chore

--- packages/i18next/locales/ru/frontend/common.js
+++ packages/i18next/locales/ru/frontend/common.js
@@ -5,6 +5,9 @@ export default {
   not_found_description: 'Не слышала о таком, даю тебе второй шанс',
   not_found_go_back: 'Обратно',
 
+  error_title: 'Произошла ошибка',
+  error_description: 'Мы о ней уже знаем и пытаемся исправить',
+
   skin_texture_variant_classic: 'Classic',
   skin_texture_variant_slim: 'Slim',
 
