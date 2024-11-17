6fbbd5bacec8cd8a5906ef8c2e0880844b490805
Egor Pronin <mrzillagold@gmail.com>
2024-11-17 19:05:53 +0900

    fix(sentry)

--- packages/backend/src/sentry.ts
+++ packages/backend/src/sentry.ts
@@ -4,6 +4,10 @@ import { IS_DEVELOPMENT_MODE } from '@skinner/constants';
 Sentry.init({
   dsn: process.env['SENTRY_DSN'],
   tracesSampleRate: 1.0,
+  // TODO: Issue with standard instrumentation:
+  // https://github.com/getsentry/sentry-javascript/issues/12891
+  // https://github.com/oven-sh/bun/issues/13165
+  defaultIntegrations: Sentry.getDefaultIntegrations({}).filter(({ name }) => name !== 'Http'),
   beforeSend: (event, hint) => {
     if (IS_DEVELOPMENT_MODE) {
       console.error(hint.originalException || hint.syntheticException);
